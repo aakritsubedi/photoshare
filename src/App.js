09e711769526415249352e59d79c0f0f43d106a7
Aakrit Subedi <aakritsubedi9@gmail.com>
2020-07-30 17:18:48 +0545

    Add Post Component and display dummy post data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-import Header from 'components/Header';
+import Header from "components/Header";
+import Post from "components/Post";
 
-import 'assets/css/App.css';
+import "assets/css/App.css";
+
+const postsData = [
+  { username: 'Aakrit Subedi', avatar: '#', image: 'http://aakritsubedi.com.np/us/img/us/aakrit.jpg', caption: 'hey .. k cha khabr?', location: 'Kathmandu, Nepal' },
+  { username: 'Aakriti Subedi', avatar: '#', image: 'http://aakritsubedi.com.np/us/img/us/aakrit.jpg', caption: 'hey .. k cha khabr?', location: 'Kathmandu, Nepal' },
+  { username: 'Yukti Bhatt', avatar: 'http://aakritsubedi.com.np/images/profile_new.jpg', image: 'http://aakritsubedi.com.np/us/img/us/aakrit.jpg', caption: 'hey .. k cha khabr?', location: 'Kathmandu, Nepal' },
+  { username: 'Aakrit Subedi', avatar: 'http://aakritsubedi.com.np/images/profile_new.jpg', image: 'http://aakritsubedi.com.np/us/img/us/aakrit.jpg', caption: 'hey .. k cha khabr?', location: 'Kathmandu, Nepal' }
+]
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    setPosts([...postsData])
+  }, [])
   return (
     <div className="App">
       <Header />
+      <div className="post-wrapper">
+        {posts.map((post, index) => (
+          <Post post={post} key={index} />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/Post/Post.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Avatar from 'react-avatar';
+
+import "assets/css/Post.css";
+
+function Post(props) {
+  const { username, avatar, image, caption, location } = props.post;
+  return (
+    <div className="post-container">
+      <div className="post-header">
+        <Avatar size="35" name={username} src={avatar} className="post-user-img" round={true}/>
+        <div className="post-header-user-info">
+          <h3 className='post-header-username'>{username}</h3>
+          <span className='post-location'>{location}</span>
+        </div>
+      </div>
+      <div className="post-img-wrapper">
+        <img src={image} alt="Aakrit Subedi" className="post-img" />
+      </div>
+      <h4 className="post-text">
+        <span className="post-username">{username}</span>&nbsp;
+        <span className="post-caption">{caption}</span>
+      </h4>
+    </div>
+  );
+}
+
+export default Post;
